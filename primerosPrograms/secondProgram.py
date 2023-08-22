7e7a98e3586b3c139a7985e985e0728b8407e894
mogollon2004 <142341315+mogollon2004@users.noreply.github.com>
2023-08-22 13:22:54 -0500

    Add files via upload

---
+++ primerosPrograms/secondProgram.py
@@ -0,0 +1,9 @@
+# Define the release and runtime integer variables below:
+release_year = 1977
+runtime = 1980
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = 50.8
+print(type(runtime))
+print(type(rating_out_of_10))
+print(type(release_year))
