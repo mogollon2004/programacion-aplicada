03062bbe4ad8dbad7dd6a4525b47b9443804c704
mogollon2004 <142341315+mogollon2004@users.noreply.github.com>
2023-09-05 13:36:44 -0500

    Create numerosPrimos.py

---
+++ Loops/numerosPrimos.py
@@ -0,0 +1,16 @@
+#El codigo evalua el rango exixtente de "i" y devuelve los nuemros primos dentro de ese rango.
+import time
+inicio = time.time()
+
+for i in range(0,10000):
+    conta = 0
+    for n in range(1, i+1):
+        residue = i%n
+        if residue == 0:
+            conta = conta + 1
+              
+    if conta == 2:
+        print(f'{i} es un primo')
+        
+fin = time.time()
+print("t = ", (fin - inicio)*1000)
