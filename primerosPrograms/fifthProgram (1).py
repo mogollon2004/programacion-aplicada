eeb761864db591a4831cd9692b13f7da8843fb46
mogollon2004 <142341315+mogollon2004@users.noreply.github.com>
2023-09-05 13:22:38 -0500

    Add files via upload

---
+++ primerosPrograms/fifthProgram (1).py
@@ -0,0 +1,23 @@
+#El valor de una varible se puede volver a redefinir tan tas veces como sea necesario
+coffee_price = 1.50
+number_of_coffees = 4
+ 
+# Prints "6.0"
+print(coffee_price * number_of_coffees)
+# Prints "1.5"
+print(coffee_price)
+# Prints "4"
+print(number_of_coffees)
+ 
+# Updating the price 
+coffee_price = 2.00
+ 
+# Prints "8.0"
+print(coffee_price * number_of_coffees)
+# Prints "2.0"
+print(coffee_price)
+# Prints "4"
+print(number_of_coffees)
+
+coffee_price = 6.00
+print(coffee_price)
